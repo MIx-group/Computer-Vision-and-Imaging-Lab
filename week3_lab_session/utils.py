39cdb4f88fdde3105a0976db41b7f0d8d49c415e
The Machine Intelligence + x group <152493097+MIx-group@users.noreply.github.com>
2024-10-14 10:48:05 +0100

    Add files via upload

---
+++ week3_lab_session/utils.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def show_binary_image(image, title=None):
+
+    # Converts from one colour space to the other. this is needed as RGB
+    # is not the default colour space for OpenCV
+
+    # Show the image
+
+    plt.imshow(image, cmap=plt.cm.gray)
+
+    # remove the axis / ticks for a clean looking image
+    plt.xticks([])
+    plt.yticks([])
+
+    # if a title is provided, show it
+    if title is not None:
+        plt.title(title)
+
+    plt.show()
